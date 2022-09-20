fe93c69e5b76ee0bbad742120a23bae0b02087ae
Anfevasa <47144203+Anfevasa@users.noreply.github.com>
2022-09-19 19:07:49 -0500

    d:3 c:2

--- api/src/routes/Controllers/postControllers.js
+++ api/src/routes/Controllers/postControllers.js
@@ -1,11 +1,22 @@
 const axios = require("axios");
 const { Dog, Temper } = require("../../db.js");
 
-const postDog = async function(req,res){
-    let dogData = req.body
+const postDog = async function (req, res) {
+  let { nombre, altura, peso, vida, img, tempers } = req.body;
 
-    await Dog.create({...dogData})
-    res.json(dogData);
-}
+  let newDog = await Dog.create({ nombre, altura, peso, vida, img });
 
-module.exports = {postDog}
+  //   console.log("modelo", Dog.__proto__)
+  //   console.log("entidad", newDog.__proto__)
+  //   console.log(newDog.addTempers instanceof Function)
+  await newDog.setTempers(tempers);
+
+  const aux = await Dog.findOne({
+    where: { nombre },
+    include: [{ model: Temper }],
+  });
+
+  res.json(aux);
+};
+
+module.exports = { postDog };
