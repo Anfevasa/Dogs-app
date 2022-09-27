f7eaf95500be9363961a5ef0b47f0545c7359357
Anfevasa <47144203+Anfevasa@users.noreply.github.com>
2022-09-26 20:42:42 -0500

    d:6 c:2

---
+++ client/src/Validators/Validators.js
@@ -0,0 +1,117 @@
+const nombreValidator = (str) => {
+  if (!/^[A-Za-z\s]*$/.test(str)) return "Nombre solo puede contener letras";
+  if (str.length > 20) return "Nombre demasiado largo";
+  if (str.length === 0) return "Nombre no puede estar vacío";
+  return false;
+};
+const minAlturaValidator = (str) => {
+  if (str.length === 0) return "Altura mínima no puede estar vacío";
+  if (!/\d/.test(str)) return "Altura solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 200) return "Altura mínima inválida";
+  return false;
+};
+const maxAlturaValidator = (str) => {
+  if (str.length === 0) return "Altura máxima no puede estar vacío";
+  if (!/\d/.test(str)) return "Altura solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 200) return "Altura máxima inválida";
+  if (document.getElementById("minAltura").value > int)
+    return "Altura máxima no puede ser menor a altura mínima ";
+  return false;
+};
+const minPesoValidator = (str) => {
+  if (str.length === 0) return "Peso mínimo no puede estar vacío";
+  if (!/\d/.test(str)) return "Peso solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 200) return "Peso mínimo inválida";
+  return false;
+};
+const maxPesoValidator = (str) => {
+  if (str.length === 0) return "Peso máximo no puede estar vacío";
+  if (!/\d/.test(str)) return "Peso solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 200) return "Peso máximo inválida";
+  if (document.getElementById("minPeso").value > int)
+    return "Peso máximo no puede ser menor a peso mínimo ";
+  return false;
+};
+const minVidaValidator = (str) => {
+  if (str.length === 0) return "Vida mínima no puede estar vacío";
+  if (!/\d/.test(str)) return "Vida solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 30) return "Vida mínima inválida";
+  return false;
+};
+const maxVidaValidator = (str) => {
+  if (str.length === 0) return "Vida máxima no puede estar vacío";
+  if (!/\d/.test(str)) return "Vida solo puede contener números";
+  let int = Number(str);
+  if (int < 1 || int > 50) return "Vida máxima inválida";
+  if (document.getElementById("minVida").value > int)
+    return "Vida máxima no puede ser menor a vida mínima ";
+  return false;
+};
+const imgValidator = (str) => {
+    if(!(/^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg|bmp)$/.test(str))) return "URL inválida"
+    return false;
+}
+
+export function validator(e) {
+    // ---- nombre -----
+  if (e.target.name == "nombre") {
+    let error = nombreValidator(e.target.value);
+    let errorLabel = document.getElementById("errorNombre");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  }
+  // ---- altura -----
+  if (e.target.name == "minAltura"){
+    let error = minAlturaValidator(e.target.value);
+    let errorLabel = document.getElementById("errorAltura");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  }
+  if (e.target.name == "maxAltura"){
+    let error = maxAlturaValidator(e.target.value);
+    let errorLabel = document.getElementById("errorAltura");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  }
+  // ---- peso -----
+  if (e.target.name == "minPeso") {
+    let error = minPesoValidator(e.target.value);
+    let errorLabel = document.getElementById("errorPeso");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  }
+  if (e.target.name == "maxPeso"){
+    let error = maxPesoValidator(e.target.value);
+    let errorLabel = document.getElementById("errorPeso");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  };
+  // ---- vida -----
+  if (e.target.name == "minVida"){
+    let error = minVidaValidator(e.target.value);
+    let errorLabel = document.getElementById("errorVida");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  };
+  // ---- IMG -----
+  if (e.target.name == "img"){
+    let error = imgValidator(e.target.value);
+    let errorLabel = document.getElementById("errorImg");
+    if (error) {
+      errorLabel.innerHTML = error;
+    } else errorLabel.innerHTML = "";
+  };
+
+  return "Tiene buena pinta";
+}
--- client/src/Vistas/CreateDog.jsx
+++ client/src/Vistas/CreateDog.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../redux/Actions";
 import { getAllTempers } from "../redux/Actions";
+import { validator } from "../Validators/Validators";
 
 export default function CreateDog() {
   const [newDog, setNewDog] = useState({
@@ -17,7 +18,9 @@ export default function CreateDog() {
   const [validDog, setValidDog] = useState(true);
 
   function handleChange(e) {
-    e.preventDefault(); 
+    e.preventDefault();
+    console.log(validator(e));
+    //console.log(e.target.name);
     setNewDog({
       ...newDog,
       [e.target.name]: e.target.value,
@@ -27,10 +30,9 @@ export default function CreateDog() {
     e.preventDefault();
     setNewDogTempers([...newDogTempers, Number(e.target.value)]);
     console.log(newDogTempers);
-    let tempersString = newDogTempers.join(", ")
-    const label = document.getElementById("labelTempers")
-    label.innerHTML = tempersString
-    
+    let tempersString = newDogTempers.join(", ");
+    const label = document.getElementById("labelTempers");
+    label.innerHTML = tempersString;
   }
 
   function validateDog(dogState) {
@@ -59,42 +61,64 @@ export default function CreateDog() {
       <form onSubmit={(e) => handleSubmit(e)}>
         <label>Nombre: </label>
         <input
-        placeholder=" nombre aquí"
+          placeholder=" nombre aquí"
           type="text"
           value={newDog.nombre}
           name="nombre"
           onChange={(e) => handleChange(e)}
         />
-        <label name="errorNombre"> </label>
+        <label id="errorNombre"> </label>
 
         <label> Altura (cm) </label>
         <label> min: </label>
         <input
           type="number"
           name="minAltura"
+          id="minAltura"
           onChange={(e) => handleChange(e)}
         />
         <label> max: </label>
         <input
           type="number"
           name="maxAltura"
+          id="maxAltura"
           onChange={(e) => handleChange(e)}
         />
-        <label name="errorAltura"> </label>
+        <label id="errorAltura"> </label>
 
         <label> Peso (Kg) </label>
         <label> min: </label>
-        <input type="number" name="minPeso" onChange={(e) => handleChange(e)} />
+        <input
+          type="number"
+          name="minPeso"
+          id="minPeso"
+          onChange={(e) => handleChange(e)}
+        />
         <label> max: </label>
-        <input type="number" name="maxPeso" onChange={(e) => handleChange(e)} />
-        <label name="errorPeso"> </label>
+        <input
+          type="number"
+          name="maxPeso"
+          id="maxPeso"
+          onChange={(e) => handleChange(e)}
+        />
+        <label id="errorPeso"> </label>
 
         <label> Expectativa de vida (años) </label>
         <label> min: </label>
-        <input type="number" name="minVida" onChange={(e) => handleChange(e)} />
+        <input
+          type="number"
+          name="minVida"
+          id="minVida"
+          onChange={(e) => handleChange(e)}
+        />
         <label> max: </label>
-        <input type="number" name="maxVida" onChange={(e) => handleChange(e)} />
-        <label name="errorVida"> </label>
+        <input
+          type="number"
+          name="maxVida"
+          id="maxVida"
+          onChange={(e) => handleChange(e)}
+        />
+        <label id="errorVida"> </label>
 
         <label> Imagen: </label>
         <input
@@ -102,11 +126,14 @@ export default function CreateDog() {
           value={newDog.img}
           onChange={(e) => handleChange(e)}
         />
-        <label name="errorImg"> </label>
+        <label id="errorImg"> </label>
 
         <label> Tempers: </label>
         <select name="tempers" value={0} onChange={(e) => handleTempers(e)}>
-          <option value={0} disabled > select tempers</option>
+          <option value={0} disabled>
+            {" "}
+            select tempers
+          </option>
           {dogsTempers.length ? (
             dogsTempers.map((temper) => {
               return (
@@ -119,7 +146,9 @@ export default function CreateDog() {
             <option value={0}>sin datos</option>
           )}
         </select>
-        <label name="errorTempers" id="labelTempers"> </label>
+        <label name="errorTempers" id="labelTempers">
+          {" "}
+        </label>
       </form>
     </div>
   );
